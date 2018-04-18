d27252ab207203c2981a0a33ccf9ca941d9d91f8
blubMe <fahmiirsyad10@gmail.com>
2018-04-18 20:27:32 +0700

    update front-end side

--- resources/assets/js/router/router.js
+++ resources/assets/js/router/router.js
@@ -1,12 +1,20 @@
 // default deb
 import Vue from 'vue';
 import Router from 'vue-router';
+// import VueAcl from 'vue-acl';
 
 //exec
 Vue.use(Router);
+// Vue.use(VueAcl, {
+//     init: 'public',
+//     // router: router,
+//     fail: '/error',
+//     save: true
+//   })
 
 //root routing
 import CoreComponent from './../components/Core';
+import LoginStudents from './../components/auth/students/login';
 
 export default new Router({
     routes: [

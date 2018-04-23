e5b463e2a39c16ac6218f0ff7761eb0b1921e4c5
Harpi <justharpi@gmail.com>
2018-04-23 22:36:28 +0700

    add new table

---
+++ database/migrations/2018_04_23_002559_create_admins_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+            $table->string('password');
+            $table->string('email');
+            // $table->integer('user_id');
+
+            $table->timestamps();
+        });
+
+        Schema::table('admins', function (Blueprint $table){
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
---
+++ database/migrations/2018_04_23_123133_create_study_programs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudyProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('study_programs', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('program');
+
+            $table->timestamps();
+        });
+
+        Schema::table('students', function (Blueprint $table){
+            $table->unsignedInteger('study_program_id');
+            $table->foreign('study_program_id')->references('id')->on('study_programs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('study_programs');
+    }
+}

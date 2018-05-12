282e464ec161df04377afe01dffa7fa5da287af0
blubMe <fahmiirsyad10@gmail.com>
2018-05-12 11:17:44 +0700

    Update : team page empty event

--- resources/assets/js/components/dashboards/students/team.vue
+++ resources/assets/js/components/dashboards/students/team.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="">
-      team
+  <div class="team">
+      <div class="team--empty">
+        <img src="/images/campfire.svg" alt="">
+        <h3>OH NO!</h3>
+        <p>Only you are in the camp, invite other member.</p>
+        <button class="btn btn--primary btn--done"> + Create team</button>
+      </div>
   </div>
 </template>
 <script>

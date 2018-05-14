729433c50fb143b17edb6fe2b046ffecd51643c8
blubMe <fahmiirsyad10@gmail.com>
2018-05-14 17:02:38 +0700

    Update : board student overview prototype # 1
    - improve reuse css codes
    - add 3 new colors = [ pink , gradientBlue , gradientGreen(beta) ]
    - Fix blocks css 
    - add bold style on nav-active

--- resources/assets/js/components/dashboards/students/board/overview.vue
+++ resources/assets/js/components/dashboards/students/board/overview.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="boardOverview">
-      <p>boardOverview</p>
+    <div class="overview__header">
+      <div class="overviewHeader__info">
+        <h3>Project JVNX</h3>
+        <p>Use this workflow to seamlessly co-ordinate</p>
+      </div>
+    </div>
   </div>
 </template>
 <script>

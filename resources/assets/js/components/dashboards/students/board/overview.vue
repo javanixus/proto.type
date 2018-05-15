744163ad60e6d8e93871d52b0abfcc9021aeb377
blubMe <fahmiirsyad10@gmail.com>
2018-05-15 09:13:31 +0700

    Optimize Web Render

--- resources/assets/js/components/dashboards/students/board/overview.vue
+++ resources/assets/js/components/dashboards/students/board/overview.vue
@@ -2,8 +2,16 @@
   <div id="boardOverview">
     <div class="overview__header">
       <div class="overviewHeader__info">
-        <h3>Project JVNX</h3>
+        <h3>Project JVNX <img src="/images/linked.svg"></h3>
         <p>Use this workflow to seamlessly co-ordinate</p>
+      </div>
+    </div>
+    <div class="overview__content">
+      <div class="overviewContent__core">
+        <div class="overview__progress"></div>
+      </div>
+      <div class="overviewContent__sidebar">
+        <div class="overviewSidebar__team"></div>
       </div>
     </div>
   </div>
--- storage/framework/views/2e0b7f55c5ea0a508da55e39cc70d61b89a578fd.php
+++ storage/framework/views/2e0b7f55c5ea0a508da55e39cc70d61b89a578fd.php
@@ -5,11 +5,13 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+        <!-- critical css preload -->
+        <style>
+            *{margin:0;padding:0;font-family:-apple-system,BlinkMacSystemFont,Roboto,Helvetica,Arial,sans-serif;box-sizing:border-box;letter-spacing:-.15px}html{text-size-adjust:100%;text-rendering:optimizeLegibility!important;-webkit-font-smoothing:subpixel-antialiased!important;text-shadow:rgba(0,0,0,.01) 0 0 1px}h1{color:#2e2f30}a{font-size:14px}a{text-decoration:underline;color:#999}.login{width:100%;height:100vh;display:flex}
+        </style>
         <title>Proto.type</title>
-        <!-- Fonts -->
-        <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet" type="text/css">
-        <link rel="preload" as="font" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" crossorigin>
-        <!-- Styles -->
+        <link href="<?php echo e(asset('css/app.css')); ?>" rel="preload" as="style" onload="this.rel='stylesheet'">
+        <link rel="preload" onload="this.rel='stylesheet'" as="font" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" crossorigin >
     </head>
     <body>
     <div id="app">

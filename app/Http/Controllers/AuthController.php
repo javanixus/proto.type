baeb86b8db4f4b4da6ef12cc1808767ef6e83ac4
Harpi <justharpi@gmail.com>
2018-05-17 20:48:56 +0700

    add role when loggin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use User;
 use JWTException;
 use JWTAuth;
 
@@ -17,11 +19,19 @@ class AuthController extends Controller
                 'error' => 'invalid.credentials',
                 'msg' => 'Invalid Credentials.'
             ], 400);
-    }
-    return response()->json([
-            'status' => 'success',
-            'token' => $token
-        ]);
+        }
+            $role = 0;
+        if (Auth::user()->role == 1) :
+            $role = 1;
+        elseif(Auth::user()->role == 2) :
+            $role = 2;
+        endif;
+
+        return response()->json([
+                'status' => 'success',
+                'token' => $token,
+                'role' => $role,
+            ]);
     }
 
     public function forgotpasword (Request $request)

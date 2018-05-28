55b51e457d75ca26581f4c7496e1ad5ed0813b2e
blubMe <fahmiirsyad10@gmail.com>
2018-05-28 14:52:50 +0700

    update summary list bar

--- resources/assets/js/components/dashboards/students/board/overview.vue
+++ resources/assets/js/components/dashboards/students/board/overview.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="boardOverview">
     <div class="overview__content">
-      <sidebar class="overviewContent__sidebar">
+      <aside class="overviewContent__sidebar">
         <span>Project info:</span>
         <div class="overviewSidebar-title">
           <h2>{{sidebar.title}}</h2>
@@ -18,8 +18,40 @@
         <div class="overviewSidebar-desc">
           <span>{{sidebar.desc}}</span>
         </div>
-      </sidebar>
-      <content class="overviewContent__core"></content> 
+      </aside>
+      <content class="overviewContent__core">
+        <div class="overviewCore-wrap">
+          <div class="overviewCore-summary">
+            <label for="overviewSummary">Summary:</label>
+            <div id="overviewSummary">
+              <div class="overviewSummary--item">
+                <div class="overviewSummaryItem__count">Private</div>
+                <div class="overviewSummaryItem__title">Project type</div>
+              </div>
+              <div class="overviewSummary--item">
+                <div class="overviewSummaryItem__count">4</div>
+                <div class="overviewSummaryItem__title">Members</div>
+              </div>
+              <div class="overviewSummary--item">
+                <div class="overviewSummaryItem__count">Development</div>
+                <div class="overviewSummaryItem__title">Status</div>
+              </div>
+              <div class="overviewSummary--item">
+                <div class="overviewSummaryItem__count">28 Days</div>
+                <div class="overviewSummaryItem__title">Deadline</div>
+              </div>
+              <div class="overviewSummary--item">
+                <div class="overviewSummaryItem__count" style="color: #25c782;">3/4</div>
+                <div class="overviewSummaryItem__title">Member actives</div>
+              </div>
+            </div>
+          </div>
+          <label for=""></label>
+          <div id="overviewCore-activity">
+
+          </div>
+        </div>
+      </content> 
     </div>
   </div>
 </template>
@@ -29,7 +61,7 @@ export default {
     return {
       sidebar : {
         title: 'Project Javanixus',
-        desc: 'some title it can make ur brain working some title it'
+        desc: 'flexible tools are designed for any project or workflow.'
       }
     }
   }

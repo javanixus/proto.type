2cf7e499bfe8ca247133598f244db84d2cc09225
blubMe <fahmiirsyad10@gmail.com>
2018-06-03 09:47:42 +0700

    Storage Page enhancement
    - new color to decrease iritate color brightness
    - add animation pulse notif

--- resources/assets/js/components/dashboards/students/board/storage.vue
+++ resources/assets/js/components/dashboards/students/board/storage.vue
@@ -8,7 +8,11 @@
     </div>
     <div class="boardStorage__content">
       <div v-if="notifIsShow" class="boardStorage__content--notif">
-        <span><b>Tips:</b> You can upload more than one item, drop your files on second box.</span>
+        <span>
+          <span class="iconCircle circle--heart">
+            <svg height="16" width="16" fill="currentColor" viewBox="0 0 16 16"><path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 12c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1zm1-3H7V4h2v5z"></path></svg>
+          </span>
+          <b>Tips:</b> &nbsp; You can upload more than one item, drop your files on second box.</span>
         <button class="btn--close" @click="notifClose">
                 <svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><path d="M11.586 13l-2.293 2.293a1 1 0 0 0 1.414 1.414L13 14.414l2.293 2.293a1 1 0 0 0 1.414-1.414L14.414 13l2.293-2.293a1 1 0 0 0-1.414-1.414L13 11.586l-2.293-2.293a1 1 0 0 0-1.414 1.414L11.586 13z" fill="currentColor" fill-rule="nonzero"></path></svg>
               </button>
--- resources/assets/js/components/example.vue
+++ resources/assets/js/components/example.vue
@@ -45,4 +45,6 @@
                         <span class="task-card__desc-title">{{task.name}}</span>
                       </div>
                     </div>
-</template>
+                    <!-- sample svg -->
+  <svg height="16" width="16" fill="currentColor" viewBox="0 0 16 16"><path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 12c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1zm1-3H7V4h2v5z"></path></svg>
+  </template>

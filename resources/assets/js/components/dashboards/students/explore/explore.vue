e2cdd8c664ed016bab058a4b7829590a0cec92b9
blubMe <fahmiirsyad10@gmail.com>
2018-06-29 21:30:35 +0700

    Update compass

--- resources/assets/js/components/dashboards/students/explore/explore.vue
+++ resources/assets/js/components/dashboards/students/explore/explore.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="team">
-      <div class="team--empty">
-        <img src="/images/campfire.svg" alt="">
+      <div class="team--empty" >
+        <div class="explore">
+          <img style="width: 120px;" src="/images/explore.png" alt="">
+          <img class="compass" src="/images/compass.png" alt="">
+        </div>
         <h3>Welcome to Explore</h3>
         <p>find and search the open source project you want.</p>
         <!-- <button class="btn btn--primary btn--done"> + Create team</button> -->
